d2189c762ea8b52af11fc731d03f3a9399a75cd0
Kaioru <keifuurawrs@gmail.com>
2018-06-21 15:50:11 +0800

    Update .gitignore

--- Porthole/Models/Mentor.cs
+++ Porthole/Models/Mentor.cs
@@ -5,6 +5,7 @@ namespace Porthole.Models
     {
 		public int ID { get; set; }
 		public string Name { get; set; }
+
 		public string EmailAddress { get; set; }
 		public string Password { get; set; }
     }
