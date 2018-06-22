d2aab1ee7458eaf2ee9a8a1e30e8027a4cd95ffd
Kaioru <keifuurawrs@gmail.com>
2018-06-22 18:12:40 +0800

    Implement setting option generation

--- Porthole/Settings.aspx
+++ Porthole/Settings.aspx
@@ -1,3 +1,34 @@
 ﻿<%@ Page Language="C#" MasterPageFile="Layouts/DefaultTemplate.master" Inherits="Porthole.Settings" CodeBehind="~/Settings.aspx.cs" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="cphContent" Runat="server">
+<h1>settings</h1>
+
+<div class="tile tile-centered">
+    <div class="tile-icon">
+        <img src="" class="avatar avatar-main" alt="avatar">
+    </div>
+    <div class="tile-content">
+        <h1><%= Account.EmailAddress %></h1>
+        <p class="tile-subtitle text-gray">this is the settings page where you can set all your tings!</p>
+    </div>
+</div>
+<br />
+
+<div class="divider" data-content="<%= AccountType %>"></div>
+<%@ Import Namespace="MoreLinq" %>
+<% foreach (var batch in Options.Batch(4)) { %>
+    <div class="list-columns columns">
+        <% foreach (var option in batch) { %>
+            <a href="<%= option.Redirect %>" class="col-3 tile tile-centered">
+                <div class="tile-icon">
+                    <figure class="avatar avatar-md">
+                        <img/>
+                    </figure>
+                </div>
+                <div class="tile-content">
+                    <p class="tile-title"><%= option.Option %></p>
+                </div>
+            </a>
+        <% } %>
+    </div>
+<% } %>
 </asp:Content>
--- Porthole/Settings.aspx.cs
+++ Porthole/Settings.aspx.cs
@@ -1,12 +1,98 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
+using Porthole.Models;
 
 namespace Porthole
 {
+	public partial class Settings : System.Web.UI.Page
+	{
+		public string AccountType { get; set; }
+		public IAccount Account { get; set; }
+		public List<SettingsOption> Options { get; set; } = new List<SettingsOption>();
 
-    public partial class Settings : System.Web.UI.Page
-    {
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			this.Account = (IAccount)Session["Account"];
+   
+			if (Account is Student) {
+				this.AccountType = "student";
+				this.Options.Add(new SettingsOption
+				{
+					Icon = "",
+					Option = "update profile",
+					Redirect = ""
+				});
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "update projects",
+                    Redirect = ""
+                });
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "view suggestions",
+                    Redirect = ""
+                });
+			}
 
+			if (Account is Mentor) {
+				this.AccountType = "mentor";
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "publicize portfolios",
+                    Redirect = ""
+                });
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "view suggestions",
+                    Redirect = ""
+                });
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "view mentees",
+                    Redirect = ""
+                });
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "search students",
+                    Redirect = ""
+                });
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "change password",
+                    Redirect = ""
+                });
+			}
+
+			if (Account is Parent) {
+				this.AccountType = "parent";
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "request child portfolio",
+                    Redirect = ""
+                });
+				this.Options.Add(new SettingsOption
+                {
+                    Icon = "",
+                    Option = "view messages",
+                    Redirect = ""
+                });
+			}
+		}
+
+		public class SettingsOption {
+			public string Icon { get; set; }
+			public string Option { get; set; }
+			public string Redirect { get; set; }
+		}
     }
 }
