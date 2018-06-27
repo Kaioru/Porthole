e39d1a90efcd68bb4ef94e83eec009f2ba321cb3
Kaioru <keifuurawrs@gmail.com>
2018-06-27 11:18:47 +0800

    Add admin log in

--- Porthole/Models/Admin.cs
+++ Porthole/Models/Admin.cs
@@ -1,26 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Porthole.Utils;
 
 namespace Porthole.Models
 {
-    public class Mentor : IAccount
+    public class Admin : IAccount
     {
-        [Key]
-        [Column("MentorID")]
         public int ID { get; set; }
         public string Name { get; set; }
 
-        [Column("EmailAddr")]
         public string EmailAddress { get; set; }
         public string Password { get; set; }
 
-        public ICollection<Student> Students { get; set; }
-        public ICollection<Suggestion> Suggestions { get; set; }
-
-        [NotMapped]
         public string Initials
         {
             get
